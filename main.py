6604dd62c465d422a7c79455b60ff62163abd274
Nikita <nick.nl1204@gmail.com>
2022-07-12 18:12:24 +0300

    Add Postgres

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 # rest of connection code using the connection string `uri`
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(uri, "sqlite:///car-pass.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
