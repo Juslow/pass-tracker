be314e29eaafc79c29b713b1fa59ccf93b2f7b39
Nikita <nick.nl1204@gmail.com>
2022-07-13 11:22:01 +0300

    Add Postgres

--- main.py
+++ main.py
@@ -39,12 +39,12 @@ app.config['ADMIN_PASSWORD'] = os.environ.get('ADMIN_PASSWORD')
 app.config['SECURITY_MAIL'] = os.environ.get('SECURITY_MAIL')
 app.config['SECURITY_PASSWORD'] = os.environ.get('SECURITY_PASSWORD')
 
-# uri = os.getenv("DATABASE_URL")  # or other relevant config var
-# if uri.startswith("postgres://"):
-#     uri = uri.replace("postgres://", "postgresql://", 1)
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 # rest of connection code using the connection string `uri`
-# app.config['SQLALCHEMY_DATABASE_URI'] = uri
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///car-pass.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
+# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///car-pass.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
